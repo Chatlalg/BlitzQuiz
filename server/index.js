b1a9eb0aff46bada793f1ab826066f01356a06fc
sojo06 <sajoshi06@gmail.com>
2024-10-01 13:01:16 +0530

    Initial_Backend

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+import authRoutes from "./routes/authroutes.js"
+dotenv.config()
+const app = express()
+const port = process.env.port
+const mongoUrl = process.env.MONGO_URL
+app.use(cors({
+    origin:process.env.ORIGIN,
+    methods:["GET","POST","PUT","DELETE","PATCH"],
+    credentials:true
+}));    
+app.use("/uploads/docs",express.static("uploads/docs"));
+app.use(cookieParser());
+app.use(express.json())
+
+app.use('/api/auth',authRoutes)
+
+mongoose.connect(mongoUrl).then(()=>{
+    console.log("Connected to Database Successfully");
+    
+})
+const server = app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+    
+})
